7687f107b8a62717b4ec38661a3ba84ba748cfd2
DouglasOwen94 <douglasowen94@gmail.com>
2026-07-30 15:03:10 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tradetrackr-v7';
+const CACHE = 'tradetrackr-v8';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
